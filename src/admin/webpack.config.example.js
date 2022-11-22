95d38402567d4e878e371cb28d4b28c30a1f2f0e
IlyaMatvitsevsky <matvicevsky_ilya@mail.ru>
2022-11-22 17:33:46 +0300

    init

---
+++ src/admin/webpack.config.example.js
@@ -0,0 +1,8 @@
+'use strict';
+
+/* eslint-disable no-unused-vars */
+module.exports = (config) => {
+  // Perform customizations to webpack config
+  // Important: return the modified config
+  return config;
+};
